f5273572adc07c8225bc462ed6806ba5d1cc4bad
Muehlner <muehlner.arao@gmail.com>
2016-09-26 23:29:52 -0300

    ajustes

--- credja/src/main/java/br/com/web/credja/enums/StatusContrato.java
+++ credja/src/main/java/br/com/web/credja/enums/StatusContrato.java
@@ -2,5 +2,30 @@ package br.com.web.credja.enums;
 
 public enum StatusContrato {
 
-	FECHADO, PENDENTE;
+	PAGO(1, "Pago"), ANALISE(2, "Em analise"), CANCELADO(3, "Cancelado");
+
+	Integer index;
+
+	String descricao;
+
+	StatusContrato(Integer index, String descricao) {
+		this.index = index;
+		this.descricao = descricao;
+	}
+
+	public Integer getIndex() {
+		return index;
+	}
+
+	public void setIndex(Integer index) {
+		this.index = index;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
