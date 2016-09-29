dce80e758e345b0e1f37a37386b1f8a6fc9dd34c
Muehlner <muehlner.arao@gmail.com>
2016-09-29 15:14:17 -0300

    alterações

--- credja/src/main/java/br/com/web/credja/controllers/TabelaController.java
+++ credja/src/main/java/br/com/web/credja/controllers/TabelaController.java
@@ -6,6 +6,7 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -50,6 +51,24 @@ public class TabelaController {
 		banco.getTabelas().add(tabela);
 		bancoDao.altera(banco);
 
+		return "redirect:lista";
+	}
+
+	@RequestMapping(value = "mostra")
+	public String mostra(Integer id, Model model) {
+		model.addAttribute("tabela", tabelaDao.buscaPorId(id));
+		return "alteracoes/tabela/mostra";
+	}
+
+	@RequestMapping(value = "altera")
+	public String altera(Tabela tabela) {
+		tabelaDao.altera(tabela);
+		return "redirect:lista";
+	}
+
+	@RequestMapping(value = "remove")
+	public String remove(Tabela tabela) {
+		tabelaDao.remove(tabela);
 		return "redirect:lista";
 	}
 
