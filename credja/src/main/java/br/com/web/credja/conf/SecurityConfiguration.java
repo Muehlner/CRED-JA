4d62ed3e704fe3866260ccd8ae4ae8dedc0a84b5
Muehlner <muehlner.arao@gmail.com>
2016-10-10 12:38:13 -0300

    login

--- credja/src/main/java/br/com/web/credja/conf/SecurityConfiguration.java
+++ credja/src/main/java/br/com/web/credja/conf/SecurityConfiguration.java
@@ -1,12 +1,14 @@
 package br.com.web.credja.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
-@EnableWebMvcSecurity
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -16,6 +18,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
+		http.authorizeRequests()
+				.antMatchers("/resources/**", "/data/**", "/dist/**", "/js/**", "/less/**", "/vendor/**").permitAll()
+				.anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
 	}
 }
